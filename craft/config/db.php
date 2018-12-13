c146aad73bdbe334aa9db810cc00d803b11eea3d
Jasper Vriends - V17 Development <jasper@v17.nl>
2018-12-13 15:53:21 +0100

    Update db.php

--- craft/config/db.php
+++ craft/config/db.php
@@ -18,22 +18,9 @@ return array(
 	/* LOCAL
 	---------------------------------------------------------- */
     '.local' => array(
-        'server'   => 'localhost',
-        'user'     => 'root',
-        'password' => 'root',
+        'server'   => '45.32.236.97',
+        'user'     => 'leppo',
+        'password' => 'tbjUBSm5O0Cm2g7h',
         'database' => 'leppo',
     ),
-
-    'leppo-jasper.local' => array(
-        'server'   => 'localhost',
-        'user'     => 'root',
-        'password' => '',
-        'database' => 'leppo',
-    ),
-    'local.leppo.nl' => array(
-        'server'   => 'localhost',
-        'user'     => 'root',
-        'password' => 'root',
-        'database' => 'leppo',
-    )
 );
