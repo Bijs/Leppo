37592bb19fb9b29fa5939cf95b897db53911dfac
Branko <branko@pixeldeluxe.nl>
2019-01-10 16:08:20 +0100

    Bestellen met Credits work in progress

--- craft/plugins/credits/variables/CreditsVariable.php
+++ craft/plugins/credits/variables/CreditsVariable.php
@@ -2,12 +2,6 @@
 /**
  * Credits plugin for Craft CMS
  *
- * Credits Variable
- *
- * --snip--
- * Craft allows plugins to provide their own template variables, accessible from the {{ craft }} global variable
- * (e.g. {{ craft.pluginName }}).
- *
  * https://craftcms.com/docs/plugins/variables
  * --snip--
  *
@@ -22,21 +16,6 @@ namespace Craft;
 
 class CreditsVariable
 {
-    /**
-     * Whatever you want to output to a Twig template can go into a Variable method. You can have as many variable
-     * functions as you want.  From any Twig template, call it like this:
-     *
-     *     {{ craft.credits.exampleVariable }}
-     *
-     * Or, if your variable requires input from Twig:
-     *
-     *     {{ craft.credits.exampleVariable(twigValue) }}
-     */
-    public function exampleVariable($optional = null)
-    {
-        return "And away we go to the Twig template...";
-    }
-
     public function calcCredits($credits){
         $credits = $credits*0.8;
 
@@ -45,5 +24,83 @@ class CreditsVariable
         }
 
         return round($credits);
+    }
+
+    public function createOrder()
+    {
+        $order = new Commerce_OrderModel();
+
+        $order->isCompleted = true;
+        $order->totalPrice = 1337;
+        $order->totalPaid = 1337;
+        $order->orderStatusId = 1;
+
+        $order->dateCreated = new DateTime('NOW');
+        $order->dateOrdered = new DateTime('NOW');
+        $order->datePaid = new DateTime('NOW');
+
+        $order->lastIp = craft()->request->getIpAddress();
+        $order->orderLocale = craft()->language;
+
+        // Right now, orders are all stored in the default currency
+        $order->currency = 'EUR';
+
+        // if (defined('COMMERCE_PAYMENT_CURRENCY'))
+        // {
+        //     $currency = StringHelper::toUpperCase(COMMERCE_PAYMENT_CURRENCY);
+        //     if (in_array($currency, $currencies))
+        //     {
+        //         $order->paymentCurrency = $currency;
+        //     }
+        // }
+
+        // // If payment currency is not set or not available anymore, default to the primary currency
+        // if (!in_array($order->paymentCurrency, $currencies))
+        // {
+        //     $order->paymentCurrency = craft()->commerce_paymentCurrencies->getPrimaryPaymentCurrencyIso();
+        // }
+
+        // Does a plugin want to set a default shipping address
+        if(!$order->shippingAddressId) {
+            $defaultShippingAddress = craft()->plugins->callFirst('commerce_defaultCartShippingAddress', [$order]);
+            if ($defaultShippingAddress) {
+                $order->setShippingAddress($defaultShippingAddress);
+            }
+        }
+
+        if (!$order->billingAddressId) {
+            $defaultBillingAddress = craft()->plugins->callFirst('commerce_defaultCartBillingAddress', [$order]);
+            if ($defaultBillingAddress) {
+                $order->setBillingAddress($defaultBillingAddress);
+            }
+        }
+
+        if ($autoSetAddresses = craft()->config->get('autoSetNewCartAddresses', 'commerce') && $customer = craft()->commerce_customers->getCustomerById($order->customerId))
+        {
+            if (
+                !$order->shippingAddressId &&
+                ($lastShippingAddressId = $customer->lastUsedShippingAddressId) &&
+                ($address = craft()->commerce_addresses->getAddressById($lastShippingAddressId))
+            ) {
+                $order->setShippingAddress($address);
+            }
+
+            if (
+                !$order->billingAddressId &&
+                ($lastBillingAddressId = $customer->lastUsedBillingAddressId) &&
+                ($address = craft()->commerce_addresses->getAddressById($lastBillingAddressId))
+            ) {
+                $order->setBillingAddress($address);
+            }
+        }
+
+        // Update the cart if the customer has changed and recalculate the cart.
+        $order->customerId = 1;
+
+        $order->billingAddressId = null;
+        $order->shippingAddressId = null;
+        return craft()->commerce_orders->saveOrder($order);
+
+        // return $order;
     }
 }
